a4cd561caf60b512a74150bdb2b049914c4b2b94
chriscompose <chriscadelga@gmail.com>
2023-12-02 22:03:39 +0000

    Implement puzzle for Day 2 and rename some variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,50 @@
 import days.DayOne
+import days.DayTwo
 import java.io.File
 
 fun main() {
+
+    val path = "/Users/christiancano/Desktop/advent-of-code/inputs"
 
     /**
      * DAY 1 - Calibration Document for Elves
      */
 
     // PART ONE
-    val listOfStrings = mutableListOf<String>()
-    val path = "/Users/christiancano/Desktop/advent-of-code"
+    val calibrationList = mutableListOf<String>()
     val dayOne = DayOne()
 
     File("$path/d1p1.txt").useLines { lines ->
         lines.forEach {
-            listOfStrings.add(it)
+            calibrationList.add(it)
         }
     }
-    println("Calibration Code for first puzzle: " + dayOne.puzzleDayOne(listOfStrings))
+    println("Calibration Code for first puzzle: " + dayOne.puzzleDayOne(calibrationList))
 
     // PART TWO
-    val listOfStringsTwo = mutableListOf<String>()
+    val calibrationListTwo = mutableListOf<String>()
 
     File("$path/d1p2.txt").useLines { lines ->
         lines.forEach {
             val replacedLine = dayOne.replaceWordNumbers(it)
-            listOfStringsTwo.add(replacedLine)
+            calibrationListTwo.add(replacedLine)
         }
     }
-    println("Calibration Code for second puzzle: " + dayOne.puzzleDayOne(listOfStringsTwo))
+    println("Calibration Code for second puzzle: " + dayOne.puzzleDayOne(calibrationListTwo))
 
     /**
-     *  DAY 2 - Title
+     *  DAY 2 - Colour Cubes
      */
 
     // PART ONE
+    val gamesList = mutableListOf<String>()
+    val dayTwo = DayTwo()
+
+    File("$path/d2p1.txt").useLines { lines ->
+        lines.forEach {
+            gamesList.add(it)
+        }
+    }
+    println("Total sum of legit games: " + dayTwo.puzzleDayTwo(gamesList))
+
 }
